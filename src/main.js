33094f69037e883612e7c5e03fa91cc4d871e023
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-04-29 11:55:19 -0400

    need to create function for clickable li

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ window.onload = init;
 function init() {
   document.querySelector("#get").addEventListener("click", getTodos);
   document.querySelector("#post").addEventListener("click", postTodo);
-  document.querySelector("#put").addEventListener("click", updateThirdTodo);
+  document.querySelector("#put").addEventListener("click", completeTodo);
 }
 
 function getTodos(event) {
@@ -13,11 +13,12 @@ function getTodos(event) {
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.onload = handleData;
   xhr.send();
+  
 }
 
 function postTodo(event) {
   event.preventDefault();
-  const userInput = document.querySelector('#todo-input').value;
+  const userInput = document.querySelector("#todo-input").value;
   //value grabbed
 
   const newTodo = {
@@ -25,21 +26,27 @@ function postTodo(event) {
     completed: false
   };
   newTodo.text = userInput;
+  
 
   const jsonnedTodo = JSON.stringify(newTodo);
-  document.querySelector('#todo-input').value = '';
+  document.querySelector("#todo-input").value = "";
   const xhr = new XMLHttpRequest();
   xhr.open("POST", "http://localhost:3000/todos");
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onload = handleData;
+  xhr.onload = getTodos;
   xhr.send(jsonnedTodo);
+ 
 }
 
-function updateThirdTodo(event) {
+function completeTodo(event) {
+  document.querySelector()
   const updatedTodo = {
     text: "????",
     completed: true
   };
+  //this.style.textDecoration = 'line-through'
+  
+
   const jsonnedTodo = JSON.stringify(updatedTodo);
 
   const xhr = new XMLHttpRequest();
@@ -49,10 +56,26 @@ function updateThirdTodo(event) {
   xhr.send(jsonnedTodo);
 }
 
-function handleData(event, value) {
+function handleData(event) {
   const todos = JSON.parse(event.target.responseText);
   console.log(todos)
-  Object.entries(value);
-
-
+  clearUl();
+  appendToUl(todos);
 }
+function appendToUl(value) {
+    value.forEach( data => {
+    const li = document.createElement("li");
+    const ul = document.querySelector("ul");
+    li.innerText = data.text;
+    ul.appendChild(li);
+  });
+}
+function clearUl(){
+  const ul = document.querySelector('ul')
+  while (ul.hasChildNodes()){
+    ul.firstChild.remove();
+  }
+}
+
+//need to create eventlistener for li 
+//apply strikethrough when clicked
